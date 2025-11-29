39811a20e509285463a3f899e57a0ea8069a45e2
quochuykzh <lehuy6c@gmail.com>
2025-11-29 20:56:18 +0700

    fix bug

---
+++ app/src/main/java/com/example/quanlythuchi_app/StatisticalChartActivity.java
@@ -0,0 +1,89 @@
+package com.example.quanlythuchi_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.quanlythuchi_app.Fragment.Chart.ChartViewAdapter;
+import com.example.quanlythuchi_app.Helper.LoadingDialog;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class StatisticalChartActivity extends AppCompatActivity {
+
+    LoadingDialog loadingDialog = new LoadingDialog(StatisticalChartActivity.this);
+    BottomNavigationView navbar;
+
+    private TabLayout tabChart;
+
+    private ViewPager2 viewPager2;
+
+    private ChartViewAdapter chartViewAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistical_chart);
+
+        navbar = findViewById(R.id.navbar);
+        navbar.setSelectedItemId(R.id.report);
+        tabChart = findViewById(R.id.tabLayoutChart);
+        viewPager2 = findViewById(R.id.viewPagerChart);
+
+        chartViewAdapter = new ChartViewAdapter(this);
+        viewPager2.setAdapter(chartViewAdapter);
+
+        new TabLayoutMediator(tabChart, viewPager2, (tab, position) -> {
+            switch (position)
+            {
+                case 0:
+                    tab.setText("Hàng tháng");
+                    break;
+                case 1:
+                    tab.setText("Theo thẻ");
+                    break;
+                case 2:
+                    tab.setText("Theo danh mục");
+                    break;
+            }
+        }).attach();
+
+
+        navbar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    int itemId = item.getItemId();
+
+                    if (itemId == R.id.report) {
+                        return true;
+                    } else if (itemId == R.id.home) {
+                        loadingDialog.startLoadingDialog();
+                        loadingDialog.setDialogDuration(2000);
+                        startActivity(new Intent(StatisticalChartActivity.this, HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    } else if (itemId == R.id.card) {
+                        loadingDialog.startLoadingDialog();
+                        loadingDialog.setDialogDuration(2000);
+                        startActivity(new Intent(StatisticalChartActivity.this, CardIntroductionActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    } else if (itemId == R.id.setting) {
+                        loadingDialog.startLoadingDialog();
+                        loadingDialog.setDialogDuration(2000);
+                        startActivity(new Intent(StatisticalChartActivity.this, SettingActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    }
+                return false;
+            }
+        });
+    }
+}

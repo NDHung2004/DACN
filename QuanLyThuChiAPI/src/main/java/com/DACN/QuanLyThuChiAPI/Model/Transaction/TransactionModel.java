a36e41f337d4d9a5efa75e35ac79c814d9b51c8d
huuvietzz <vietvip01012004@gmail.com>
2025-12-02 12:05:25 +0700

    Thống kê update

--- QuanLyThuChiAPI/src/main/java/com/DACN/QuanLyThuChiAPI/Model/Transaction/TransactionModel.java
+++ QuanLyThuChiAPI/src/main/java/com/DACN/QuanLyThuChiAPI/Model/Transaction/TransactionModel.java
@@ -16,5 +16,42 @@ import java.sql.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private UserInfoModel userInfoModel;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private CategoryModel categoryModel;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "card_id")
+    private CardModel cardModel;
+
+
+    @Column(name = "name")
+    private String name;
+
+
+    @Column(name = "amount")
+    private Long amount;
+
+    @Column(name = "location")
+    private String location;
+
+
+    @Column(name = "transactiondate")
+    private Date transactiondate;
+
+    // the loai 1 = thu vào, 2 = chi tiêu
+    @Column(name = "type")
+    private Long type;
+
+    @Column(name = "description")
+    private String description;
 }
